0a281d70d871fdcb3743c5b0590ad00d52879fbb
stormi186 <jasna.havic@gmail.com>
2017-07-28 14:23:06 +0200

    Implemented advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
 
   ROLE_USER = 0
   ROLE_ADMIN = 1
@@ -15,6 +16,30 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def User.digest(string)
+  	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
+  	BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+  	SecureRandom.urlsafe_base64
+  end
+
+  def remember
+  	self.remember_token = User.new_token
+  	update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+  	return false if remember_digest.nil?
+  	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+  	update_attribute(:remember_digest, nil)
+  end
 
   def admin?
     role == ROLE_ADMIN
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727100834) do
+ActiveRecord::Schema.define(version: 20170728115757) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name", null: false
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20170727100834) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.string "remember_digest"
   end
 
 end
