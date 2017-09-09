161b77fa490de2e111d34313e2477635d73a9fb1
stormi186 <jasna.havic@gmail.com>
2017-09-09 20:35:19 +0200

    changing group by for pg

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -4,7 +4,7 @@ class TracksController < ApplicationController
 	
 	def index
   	@tracks = Track.search(params[:term]).paginate(:page => params[:page], :per_page => 10).order(created_at: :desc)
-  	@top_tracks = Favorite.joins("LEFT OUTER JOIN tracks ON favorites.track_id = tracks.id").select("favorites.*,tracks.name as name, tracks.artist_id as artist_id").select(:track_id).group(:track_id).order('COUNT(tracks.id) DESC')
+  	@top_tracks = Favorite.joins("LEFT OUTER JOIN tracks ON favorites.track_id = tracks.id").select("favorites.*,tracks.name as name, tracks.artist_id as artist_id").select("track_id").group("track_id").order('COUNT(tracks.id) DESC')
   .limit(10)
   	@latest_albums = Album.order('created_at DESC').last(5)
 	end
