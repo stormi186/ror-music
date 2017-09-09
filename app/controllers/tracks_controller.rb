8e5b1eb2e5ee5cd491220d515ed5cc30fd4d8f9e
stormi186 <jasna.havic@gmail.com>
2017-09-09 22:48:34 +0200

    pg

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -6,7 +6,7 @@ class TracksController < ApplicationController
   	@tracks = Track.search(params[:term]).paginate(:page => params[:page], :per_page => 10).order(created_at: :desc)
   	#@top_tracks = Favorite.joins("LEFT OUTER JOIN tracks ON favorites.track_id = tracks.id").select("favorites.*,tracks.name as name, tracks.artist_id as artist_id").group(:track_id).order('COUNT(tracks.id) DESC').limit(10)
   	#@top_tracks = Favorite.joins("LEFT OUTER JOIN tracks ON favorites.track_id = tracks.id").select("favorites.*,tracks.name as name, tracks.artist_id as artist_id").select("track_id").group("track_id").order('COUNT(tracks.id) DESC').limit(10)
-    @top_tracks = Favorite.joins('join tracks on favorites.track_id = tracks.id').group('track_id').select('id, track_id, name, artist_id').order('COUNT(tracks.id) DESC').limit(10)
+    @top_tracks = Favorite.joins('join tracks on favorites.track_id = tracks.id').select('track_id, name, artist_id').group('track_id').order('COUNT(favorites.id) DESC').limit(10)
   	@latest_albums = Album.order('created_at DESC').last(5)
 	end
 
