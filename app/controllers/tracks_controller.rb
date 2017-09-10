a8ee868c7baf5b3a022d10950baa36beba4f4500
stormi186 <jasna.havic@gmail.com>
2017-09-10 11:15:23 +0200

    sql version

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -5,7 +5,7 @@ class TracksController < ApplicationController
 	def index
   	@tracks = Track.search(params[:term]).paginate(:page => params[:page], :per_page => 10).order(created_at: :desc)
   	#lokalno radi
-  	#@top_tracks = Favorite.joins("LEFT OUTER JOIN tracks ON favorites.track_id = tracks.id").select("favorites.*,tracks.name as name, tracks.artist_id as artist_id").group(:track_id).order('COUNT(tracks.id) DESC').limit(10)
+  	@top_tracks = Favorite.joins("LEFT OUTER JOIN tracks ON favorites.track_id = tracks.id").select("favorites.*,tracks.name as name, tracks.artist_id as artist_id").group(:track_id).order('COUNT(tracks.id) DESC').limit(10)
     #ne radi sa pg
     #@top_tracks = Favorite.joins('join tracks on favorites.track_id = tracks.id').select('favorites.id, favorites.user_id, favorites.track_id, favorites.created_at, favorites.updated_at, tracks.name, tracks.artist_id').group('track_id').order('COUNT(tracks.id) DESC').limit(10)
   	@latest_albums = Album.order('created_at DESC').last(5)
